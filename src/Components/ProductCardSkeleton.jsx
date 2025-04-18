d5374fa083ba36d966e07f78fbff03c2e3964eb3
captain-adi <maniramp554@gmail.com>
2025-04-18 20:26:16 +0530

    change height of skeleton

--- src/Components/ProductCardSkeleton.jsx
+++ src/Components/ProductCardSkeleton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProductCardSkeleton() {
   return (
     <>
-      <div class="w-64 border rounded-lg bg-white p-4">
+      <div class="w-64 h-48 border rounded-lg bg-white p-4">
         <div class="w-full h-48 overflow-hidden rounded-md bg-gray-200 animate-pulse"></div>
         <div class="flex flex-col flex-grow mt-3">
           <div class="h-6 bg-gray-200 rounded-md animate-pulse w-3/4"></div>
